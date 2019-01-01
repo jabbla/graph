db11f3431e221c712e579a472560b6e04c24340d
jabbla <gloria_n@yeah.net>
2019-01-01 17:23:01 +0800

    fix: add build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,8 @@
-import minify from 'rollup-plugin-babel-minify';
-
-const env = process.env.NODE_ENV;
-const outputFile = env === 'dev'? 'docs/dist/graph.js':'dist/graph.min.js';
-const devPlugins = [];
-const prodPlugins = [minify({
-    comments: false,
-    sourceMap: true,
-    banner: undefined,
-    bannerNewLine: false
-})];
-const plugins = env === 'dev'? devPlugins : prodPlugins;
+const outputFile = 'docs/dist/graph.js';
 export default {
     input: 'src/index.js',
     output: {
         file: outputFile,
         format: 'cjs'
-    },
-    plugins
+    }
 };
---
+++ scripts/build.js
@@ -0,0 +1,48 @@
+const rollup = require('rollup');
+const minify = require('rollup-plugin-babel-minify');
+
+const inputOptions = [
+    {
+        input: 'src/index.js',
+    },
+    {
+        input: 'src/index.js',
+        plugins: [minify({
+            comments: false,
+            sourceMap: true,
+            banner: undefined,
+            bannerNewLine: false
+        })]
+    }
+];
+
+const outputOptions = [
+    {
+        file: 'docs/dist/graph.js',
+        format: 'cjs'
+    },
+    {
+        file: 'dist/graph.js',
+        format: 'cjs'
+    },
+    {
+        file: 'dist/graph.min.js',
+        format: 'cjs'
+    }
+]
+
+async function build() {
+    const bundle = await rollup.rollup(inputOptions[0]);
+    await bundle.generate(outputOptions[0]);
+    await bundle.write(outputOptions[0]);
+
+    await bundle.generate(outputOptions[1]);
+    await bundle.write(outputOptions[1]);
+
+
+    const bundleMin = await rollup.rollup(inputOptions[1]);
+    await bundleMin.generate(outputOptions[2]);
+    await bundleMin.write(outputOptions[2]);
+}
+
+build();
