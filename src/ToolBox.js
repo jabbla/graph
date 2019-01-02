a1a93b236b982d152b8dddbe8b3e8946c8c026a5
jabbla <gloria_n@yeah.net>
2019-01-02 10:25:27 +0800

    fix: fix toolBox style

--- src/ToolBox.js
+++ src/ToolBox.js
@@ -18,42 +18,6 @@ const Icons = {
 class ToolBox {
     constructor(panZoomTiger){
         this.panZoomTiger = panZoomTiger;
-        this.createCSSText();
-    }
-    createCSSText(){
-        const style = document.createElement('style');
-
-        style.innerHTML = `
-            .graph_toolbox {
-                font-size: 12px;
-                margin: 0;
-                transform: translateY(-100%);
-                padding: 10px;
-            }
-            .graph_toolbox .tooltip {
-                display: flex;
-                width: 100%;
-                align-item: center;
-                justify-content: center;
-            }
-            .graph_toolbox .btn {
-                display: flex;
-                align-items: center;
-                line-height: 32px;
-                position: relative;
-                z-index: 10;
-                width: 32px;
-                height: 32px;
-                cursor: pointer;
-                text-align: center;
-                color: #fff;
-                border: 1px solid #dedede;
-                background: #fff;
-                margin-bottom: 4px;
-            }
-        `;
-
-        document.head.appendChild(style);
     }
     create(svgStr){
         const svg = document.querySelector(svgStr);
@@ -67,17 +31,15 @@ class ToolBox {
             position: 'fixed',
             left: clientRect.left + 'px',
             top: clientRect.bottom + 'px',
-            transform: 'translateY()'
+            fontSize: '12px',
+            margin: '0',
+            transform: 'translateY(-100%)',
+            padding: '10px'
         });
 
         this.build(zoomInBtn, 'zoomIn');
         this.build(zoomOutBtn, 'zoomOut');
         this.build(resetBtn, 'resetZoom');
-
-        toolBox.className = 'graph_toolbox';
-        zoomInBtn.className = 'btn graph_zoomInBtn';
-        zoomOutBtn.className = 'btn graph_zoomOutBtn';
-        resetBtn.className = 'btn graph_resetBtn';
 
         toolBox.appendChild(zoomInBtn);
         toolBox.appendChild(zoomOutBtn);
@@ -94,7 +56,11 @@ class ToolBox {
         let icon = document.createElement('span');
 
         icon.innerHTML = Icons[type];
-        icon.className = 'tooltip';
+
+        Object.assign(icon.style, {
+            display: 'flex',
+            alignItems: 'center'
+        });
 
         const tooltip = new ToolTip('node');
         let text = '';
@@ -111,6 +77,23 @@ class ToolBox {
         default:
             text = '放大';
         }
+
+        Object.assign(li.style, {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            lineHeight: '32px',
+            position: 'relative',
+            zIndex: '10',
+            width: '32px',
+            height: '32px',
+            cursor: 'pointer',
+            textAlign: 'center',
+            color: '#fff',
+            border: '1px solid #dedede',
+            background: '#fff',
+            marginBottom: '4px'
+        });
 
         li.addEventListener('click', () => {
             this.panZoomTiger[type]();
